afa209012b4541344b8f40e5d7e5b5b29a3a3aa6
Andrew Schell <schell.116@buckeyemail.osu.edu>
2025-03-28 15:19:41 -0400

    catalog controller added updates, items and deletes

--- Amber-Sun.Api/Controllers/CatalogController.cs
+++ Amber-Sun.Api/Controllers/CatalogController.cs
@@ -19,5 +19,37 @@ namespace Amber.Sun.Api.Controllers
             return Ok(items);
         }
 
-    
+        [HttpGet("{id:int}")]
+
+        public IActionResult GetItem(int id)
+        {
+            var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
+            item.Id = id;
+            return Ok(item);   
+}
+
+        [HttpPost]
+        public IActionResult Post(Item item){
+    return Created("catalog/42", item);
+}
+
+        [HttpPost("{id}/ratings")]
+        public IActionResult PostRating(int id, [From Body] Rating rating)
+        {
+            var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
+            item.Id = id;
+            item.AddRating(rating);
+            return Ok(item);
+        }
+        [httpput]
+
+        public IActionResult Put(int id, Item item){
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
 }
