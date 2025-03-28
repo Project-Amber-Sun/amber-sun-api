5dc77f0ae85d8d58b995aac09beeb5ccc98d31de
Andrew Schell <schell.116@buckeyemail.osu.edu>
2025-03-28 14:47:15 -0400

    Adding API and CatalogController.cs(PP.8)

--- Amber-Sun.Api/Controllers/CatalogController.cs
+++ Amber-Sun.Api/Controllers/CatalogController.cs
@@ -1,3 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Amber.Sun.Domain.Catalog;
 
+namespace Amber.Sun.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class CatalogController : ControllerBase
+    [HttpGet]
+    {
+        public IActionResult GetItems(){
+            return Ok("Hello World!")
+        }
+    
+}
+}
