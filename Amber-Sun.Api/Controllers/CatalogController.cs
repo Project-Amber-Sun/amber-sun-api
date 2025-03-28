895490552414ae05886f170e836bac0d41dfd37e
Andrew Schell <schell.116@buckeyemail.osu.edu>
2025-03-28 15:36:41 -0400

    Initial commit of the ef core branch

--- Amber-Sun.Api/Controllers/CatalogController.cs
+++ Amber-Sun.Api/Controllers/CatalogController.cs
@@ -5,45 +5,45 @@ namespace Amber.Sun.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-
     public class CatalogController : ControllerBase
-    [HttpGet]
     {
-        public IActionResult GetItems(){
+        [HttpGet]
+        public IActionResult GetItems()
+        {
             var items = new List<Item>()
             {
                 new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m),
                 new Item("Shorts", "Ohio State Shorts", "Nike", 49.99m),
-        };
             };
             return Ok(items);
         }
 
         [HttpGet("{id:int}")]
-
         public IActionResult GetItem(int id)
         {
             var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
             item.Id = id;
-            return Ok(item);   
-}
+            return Ok(item);
+        }
 
         [HttpPost]
-        public IActionResult Post(Item item){
-    return Created("catalog/42", item);
-}
+        public IActionResult Post(Item item)
+        {
+            return Created("catalog/42", item);
+        }
 
         [HttpPost("{id}/ratings")]
-        public IActionResult PostRating(int id, [From Body] Rating rating)
+        public IActionResult PostRating(int id, [FromBody] Rating rating)
         {
             var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
             item.Id = id;
             item.AddRating(rating);
             return Ok(item);
         }
-        [httpput]
 
-        public IActionResult Put(int id, Item item){
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Item item)
+        {
             return NoContent();
         }
 
@@ -52,4 +52,5 @@ namespace Amber.Sun.Api.Controllers
         {
             return NoContent();
         }
+    }
 }
