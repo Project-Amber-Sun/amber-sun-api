213ba721d87bb4b846940bbdd4a03fc7a70a8ee4
Andrew Schell <schell.116@buckeyemail.osu.edu>
2025-04-20 21:28:07 -0400

    added security into catalog controller

--- Amber-Sun.Api/Program.cs
+++ Amber-Sun.Api/Program.cs
@@ -2,13 +2,32 @@ using Amber.Sun.Data;
 using Microsoft.EntityFrameworkCore;
 
 using Microsoft.EntityFrameworkCore.Sqlite;
+using Amber.Sun.Api.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+string authority = builder.Configuration["AzureAd:Instance"] ??
+throw new ArgumentNullException("Auth0:Authority");
+
+string audience = builder.Configuration["AzureAd:Audience"] ??
+throw new ArgumentNullException("Auth0:Audience");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
+{
+    Options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    Options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Authority = authority;
+    options.Audience = audience;
+});
+
 builder.Services.AddDbContext<StoreContext>(options => options.UseSqlite("Data Source =../Registrar.sqlite",
 b => b.MigrationsAssembly("Amber-Sun.Api"))
 );
@@ -22,7 +41,12 @@ Builder.services.AddCors(Options =>
         .AllowAnyMethod();
         });
 });
-
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("delete:catalog", policy =>
+    {
+        policy.RequirementAuthenticatedUser()
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -41,7 +65,7 @@ app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
